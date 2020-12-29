f4714b584e5881b4f2f6cda8dbd2d98c6b37da58
Millad Ayoubi <Milladayoubi@gmail.com>
2020-12-29 00:21:36 -0500

    UpdateOne Document From Collection Users

--- updateUser.js
+++ updateUser.js
@@ -1,12 +1,32 @@
-const update = (db) => {
+const { ObjectID } = require("mongodb")
 
+const update = (db) => {
+    console.log( 'Updating Millad ')
 
     db.collection('users').updateOne({
+        name: 'Miguel'
+
+    }, {
+        $set: {
+            name: 'Miguel', 
+            program: 'Cobra Kai',
+            university: 'None'
+        },
 
 
+        $inc: {
+            age: -11
+        }
 
         
+        
+    }).then((result) => {
+        console.log(result)
+
+    }).catch((error) => {
+        console.log(error)
     })
+
 }
 
 
