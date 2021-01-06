99e4d7fbe450644d0e3733d8719251487397bd71
Millad Ayoubi <Milladayoubi@gmail.com>
2021-01-06 13:50:08 -0500

    Mongoose NPM

---
+++ src/mongoose.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+
+})
+
+
+
+
+//Mongoose Object Model Created
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+
+    age: {
+        type: Number
+    }
+
+
+})
+
+//Creating New Object 'sensai'
+const sensai = new User({
+    name: 'Daniel LaRusso',
+    age: 41
+})
+
+sensai.save().then((sensai) => {
+
+}).catch((error) => {
+    console.log('Unexpected Error!', error)
+})
